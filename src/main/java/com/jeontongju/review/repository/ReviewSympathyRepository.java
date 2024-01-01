d07232be2e3dffdf7561a0e1fd35c2fa953096ea
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2024-01-02 08:36:07 +0900

    FEAT
    GET PRODUCT REVIEW
    
    SCRUM-176

--- src/main/java/com/jeontongju/review/repository/ReviewSympathyRepository.java
+++ src/main/java/com/jeontongju/review/repository/ReviewSympathyRepository.java
@@ -1,7 +1,10 @@
 package com.jeontongju.review.repository;
 
+import com.jeontongju.review.domain.Review;
 import com.jeontongju.review.domain.ReviewSympathy;
 import com.jeontongju.review.domain.ReviewSympathyId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewSympathyRepository extends JpaRepository<ReviewSympathy, ReviewSympathyId> {}
+public interface ReviewSympathyRepository extends JpaRepository<ReviewSympathy, ReviewSympathyId> {
+    Long countByReviewId(Review review);
+}
--- src/main/java/com/jeontongju/review/service/ReviewService.java
+++ src/main/java/com/jeontongju/review/service/ReviewService.java
@@ -3,11 +3,13 @@ package com.jeontongju.review.service;
 import com.jeontongju.review.client.ConsumerServiceClient;
 import com.jeontongju.review.client.OrderServiceClient;
 import com.jeontongju.review.client.ProductServiceClient;
+import com.jeontongju.review.domain.Review;
 import com.jeontongju.review.domain.ReviewSympathy;
 import com.jeontongju.review.domain.ReviewSympathyId;
 import com.jeontongju.review.dto.request.CreateReviewDto;
 import com.jeontongju.review.dto.response.GetMyReviewDto;
 import com.jeontongju.review.dto.response.GetReviewDto;
+import com.jeontongju.review.dto.response.ReviewContentsDto;
 import com.jeontongju.review.exception.ReviewNotFoundException;
 import com.jeontongju.review.exception.common.CustomFailureException;
 import com.jeontongju.review.kafka.ReviewProducer;
@@ -15,11 +17,17 @@ import com.jeontongju.review.mapper.ReviewMapper;
 import com.jeontongju.review.repository.ReviewRepository;
 import com.jeontongju.review.repository.ReviewSympathyRepository;
 import com.jeontongju.review.repository.ReviewTagRepository;
+import com.jeontongju.review.repository.querydsl.ReviewCustomRepository;
 import io.github.bitbox.bitbox.dto.ConsumerNameImageDto;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.bitbox.bitbox.enums.FailureTypeEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +46,7 @@ public class ReviewService {
   private final OrderServiceClient orderServiceClient;
   private final ReviewMapper reviewMapper;
   private final ReviewProducer reviewProducer;
+  private final ReviewCustomRepository reviewCustomRepository;
 
   @Transactional
   public void createReview(Long memberId, CreateReviewDto createReviewDto) {
@@ -79,5 +88,40 @@ public class ReviewService {
 
       reviewSympathyRepository.save(reviewSympathy);
     }
+  }
+
+  public Page<GetMyReviewDto> getMyReview(Long memberId, Pageable pageable) {
+    return reviewRepository.findMyReviewByConsumerId(memberId, pageable);
+  }
+
+  public GetReviewDto getProductReview(String productId, Long memberId, Pageable pageable) {
+    List<ReviewContentsDto> reviewContentsDtoList = new ArrayList<>();
+
+    Page<Review> reviewList = reviewRepository.findByProductId(productId, pageable);
+    Boolean isSympathy = false;
+    for (Review r : reviewList) {
+      if (memberId != null) {
+        isSympathy =
+            reviewSympathyRepository.existsById(
+                ReviewSympathyId.builder().reviewId(r.getReviewId()).consumerId(memberId).build());
+      }
+
+      reviewContentsDtoList.add(
+          ReviewContentsDto.builder()
+              .reviewId(r.getReviewId())
+              .name(r.getName())
+              .profileImageUrl(r.getProfileImageUrl())
+              .reviewSympathyCount(reviewSympathyRepository.countByReviewId(r))
+              .concept(reviewTagRepository.findNameByReviewId(r.getReviewId()))
+              .createdAt(r.getCreatedAt())
+              .isSympathy(isSympathy)
+              .build());
+    }
+
+    return GetReviewDto.builder()
+        .representativeReview(
+            reviewTagRepository.findNameByProductId(productId, PageRequest.of(0, 2)))
+        .content(new PageImpl<>(reviewContentsDtoList, pageable, reviewList.getTotalElements()))
+        .build();
   }
 }
