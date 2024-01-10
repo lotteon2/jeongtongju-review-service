6226c1e6687370dfcfd12e3cb541a8a24d18ff5c
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2024-01-10 09:38:58 +0900

    FEAT
    seller review for dashboard
    
    SCRUM-363

--- src/main/java/com/jeontongju/review/repository/ReviewRepository.java
+++ src/main/java/com/jeontongju/review/repository/ReviewRepository.java
@@ -2,21 +2,19 @@ package com.jeontongju.review.repository;
 
 import com.jeontongju.review.domain.Review;
 import com.jeontongju.review.dto.response.GetMyReviewDto;
-import com.jeontongju.review.dto.response.GetReviewDto;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
   @Query(
       "select new com.jeontongju.review.dto.response.GetMyReviewDto(r.reviewId, r.productId, r.productThumbnailImage, r.contents, r.imageUrl, r.createdAt) from Review r where r.isDeleted = false and r.consumerId = :memberId")
   Page<GetMyReviewDto> findMyReviewByConsumerId(Long memberId, Pageable pageable);
-
   Page<Review> findByProductIdAndIsDeleted(String productId, Boolean isDeleted, Pageable pageable);
   List<Review> findByProductIdAndIsDeleted(String productId, Boolean isDeleted);
   List<Review> findByName(String name);
+  Page<Review> findBySellerId(Long memberId, Pageable pageable);
 }
